0eeb1a665e3c4b7e6ff66fec21ad8aa57e94a0aa
caixian <qq52577517@163.com>
2020-10-26 17:30:17 +0800

    跳关

--- assets/script/public/prefab/GoNextLevel.js
+++ assets/script/public/prefab/GoNextLevel.js
@@ -11,5 +11,13 @@ cc.Class({
 
     goNextLevel() {
         cc.log('go next');
+        const evt1 = new cc.Event.EventCustom('_toggle_loading', true);
+        evt1.setUserData({status: true});
+        this.node.dispatchEvent(evt1);
+        const evt = new cc.Event.EventCustom('_record_lv_star', true);
+        evt.setUserData({star: 1});
+        this.node.dispatchEvent(evt);
+        this.node.dispatchEvent(new cc.Event.EventCustom('_unlock_lv', true));
+        this.node.dispatchEvent(new cc.Event.EventCustom('_go_next_lv', true));
     },
 });
