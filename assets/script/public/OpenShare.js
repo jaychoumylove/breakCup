0eddc846a9205d29c678b02494f63a48875d3d7f
caixian <qq52577517@163.com>
2020-10-29 15:26:58 +0800

    加入分享

---
+++ assets/script/public/OpenShare.js
@@ -0,0 +1,35 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        platform: cc.String
+    },
+
+    onLoad () {
+        // this.platformMap = {
+        //     'wechat': wx
+        // };
+        // this.platformObject = this.platformMap[this.platform];
+        if (this.platform == 'wechat') {
+            this.openWxShare();
+        }
+    },
+
+    // update (dt) {},
+
+    openWxShare() {
+        wx.showShareMenu({withShareTicket:true});
+        wx.onShareAppMessage(function(res){
+            return {
+                title: "经典火柴人游戏始终好玩，来吧！一起回味经典的乐趣。",
+                imageUrl: null,
+                success(res){
+                    console.log(res)
+                },
+                fail(res){
+                    console.log(res)
+                } 
+            }
+        })
+    },
+});
